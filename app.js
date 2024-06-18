c41ebde02fe027f890b3ade783363647ca81874e
richiea1y <44350561+richiea1y@users.noreply.github.com>
2024-06-18 14:37:35 +0800

    refactor

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from 'url';
-import { Sequelize } from "sequelize";
-
-import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
 
 
-// 透過 new 建立 Sequelize 這個 class，而 sequelize 就是物件 instance
-const sequelize = new Sequelize('gym-db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
@@ -19,32 +11,20 @@ const app = express();
 const port = 8887;
 
 
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
+app.get("/", (req, res)=>{
+    res.send("Hello");
+})
 
-//error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}...`);
 });
 
-export default sequelize
